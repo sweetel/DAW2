b25a7220cc1dfadb247a5f46f102528f0c53ee98
sweetel <alumnofp@FPB15.CALASANZSALAMANCA.local>
2024-10-17 09:16:24 +0200

    delet

--- proyecto1/src/main/java/com/cbs/proyecto1/Controllers/PersonController.java
+++ proyecto1/src/main/java/com/cbs/proyecto1/Controllers/PersonController.java
@@ -3,6 +3,8 @@ package com.cbs.proyecto1.Controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,58 +13,79 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cbs.proyecto1.dto.PersonaDTO;
 
 @RestController
-	public class PersonController {    
+public class PersonController {
 
-	    private List<PersonaDTO> personList;
+	private List<PersonaDTO> personList;
 
-	    public PersonController() {
-	        this.personList = new ArrayList<>();
-	        this.personList.add(new PersonaDTO("654981054F", "Eduardo", "Gómez", "18", "gay"));
-	    }
+	public PersonController() {
+		this.personList = new ArrayList<>();
+		this.personList.add(new PersonaDTO("654981054F", "Eduardo", "Gómez", "18", "gay"));
+	}
 
-	    @GetMapping("/persons/list")
-	    public List<PersonaDTO> personList() {
+	@GetMapping("/persons/list")
+	public List<PersonaDTO> personList() {
 
-	        return personList;
-	       
-	    }
+		return personList;
 
-	    @PostMapping("/persons/add")
-	    public List<PersonaDTO> personsAdd(@RequestBody PersonaDTO personDTO) {
-	        
-	        // this.personList.add(new PersonDTO("1344456T", "Paco", "Carmona", 23, "lesbiano"));
-	        // this.personList.add(new PersonDTO("654981054F", "Eduardo", "Gómez", 18, "gay"));
-	        // this.personList.add(new PersonDTO("9087536X", "Joan", "Sánchez", 4, "desconocido"));
+	}
 
-	        this.personList.add(personDTO);
+	@PostMapping("/persons/add")
+	public List<PersonaDTO> personsAdd(@RequestBody PersonaDTO personDTO) {
 
-	        return personList;
-	       
-	    }
+		// this.personList.add(new PersonDTO("1344456T", "Paco", "Carmona", 23,
+		// "lesbiano"));
+		// this.personList.add(new PersonDTO("654981054F", "Eduardo", "Gómez", 18,
+		// "gay"));
+		// this.personList.add(new PersonDTO("9087536X", "Joan", "Sánchez", 4,
+		// "desconocido"));
 
-	    @PostMapping("/persons/update")
-	    public List<PersonaDTO> personsUpdate(@RequestBody PersonaDTO personDTO) {
+		this.personList.add(personDTO);
 
-	        for (PersonaDTO person : personList) {
+		return personList;
 
-	            if(person.getDNI().equals(personDTO.getDNI())) {
+	}
 
-					person.setNombre(personDTO.getNombre());
-					person.setApellido(personDTO.getApellido());
-					person.setEdad(personDTO.getEdad());
-					person.setSexo(personDTO.getSexo());
-	            }
+	@PostMapping("/persons/update")
+	public List<PersonaDTO> personsUpdate(@RequestBody PersonaDTO personDTO) {
 
-	        }
+		for (PersonaDTO person : personList) {
 
-	        return personList;
-	    }
+			if (person.getDNI().equals(personDTO.getDNI())) {
 
-	    @GetMapping("/persons/remove")
-	    public String personsRemove() {
+				person.setNombre(personDTO.getNombre());
+				person.setApellido(personDTO.getApellido());
+				person.setEdad(personDTO.getEdad());
+				person.setSexo(personDTO.getSexo());
+			}
 
-	        return null;
+		}
 
-	    }
-	    
+		return personList;
+	}
+
+	@DeleteMapping("/persons/delete")
+	public ResponseEntity<?> delete(@RequestBody PersonaDTO personDelete) {
+
+		PersonaDTO PersonAux = null;
+		try {
+		for (PersonaDTO personDTO : personList) {
+			if (personDelete.getDNI().equals(personDTO.getDNI())) {
+				PersonAux = personDTO;
+			}
+		}
+
+		if (null != PersonAux) {
+			personList.remove(PersonAux);
+			return ResponseEntity.ok(personList);
+		}
+
+		else {
+			return ResponseEntity.badRequest().body("usuario no encontrado");
+		}
+		} catch(Exception ex) {
+			return ResponseEntity.internalServerError().body("error al procesar el delete");
+		}
+
+	} 
+
 }
