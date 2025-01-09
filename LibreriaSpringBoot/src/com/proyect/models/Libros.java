7d14c603f0fb08253c690037d192a14d9bbc5100
sweetel <alumnofp@FPB15.CALASANZSALAMANCA.local>
2025-01-09 12:42:31 +0100

    rep

---
+++ LibreriaSpringBoot/src/com/proyect/models/Libros.java
@@ -0,0 +1,18 @@
+package com.proyect.models;
+
+public class Libros {
+
+	private String ISBN;
+	private String Title;
+	
+	@Id
+	@GeneratedValue(strategy )
+	
+	public Libros() {
+		super();
+	}
+	
+
+	
+	
+}
